bf22fd8394dce5a449757aefd5968596a99ccea4
Henrique Carvalho <carvalhoghenrique@gmail.com>
2025-09-17 12:37:30 -0300

    feat(Aplic.java): Incrementa a lógica da classe java principal para receber a nova classe

--- prjExemploGenerEspecComPolimorfismo/src/Aplic.java
+++ prjExemploGenerEspecComPolimorfismo/src/Aplic.java
@@ -1,6 +1,8 @@
 
 import fatec.poo.model.FuncionarioHorista;
 import fatec.poo.model.FuncionarioMensalista;
+import fatec.poo.model.FuncionarioComissionado;
+import java.text.DecimalFormat;
 
 
 /**
@@ -10,27 +12,49 @@ import fatec.poo.model.FuncionarioMensalista;
 public class Aplic {
 
     public static void main(String[] args) {
+        //DecimalFormat df = new DecimalFormat("#,##0.00"); -> Baseado no padrão da máquina
+        DecimalFormat df = new DecimalFormat("#,##0.00");
         
+        //INSTACIA E PASSA VALORES A CLASSE FuncionarioHorista
         FuncionarioHorista funcHor = new FuncionarioHorista(1010, "Pedro Silveira", "14/05/1978", 15.80);
         funcHor.setQtdeHorTrab(90);
         funcHor.setCargo("Gerente");
         
+        //INSTACIA E PASSA VALORES A CLASSE FuncionarioMensalista
         FuncionarioMensalista funcMen = new FuncionarioMensalista(2020, "Henrique", "06/05/2006", 1510.0);
         funcMen.setNumSalMin(3);
         funcMen.setCargo("Estagiário");
         
-        System.out.println("HORÁRIO");
-        System.out.println("Cargo: [" + funcHor.getCargo() + "]");
-        System.out.println("Salario Bruto: [" + funcHor.calcSalBruto() + "]");
-        System.out.println("Desconto: [" + funcHor.calcDesconto() + "]");
-        System.out.println("Gratificação: [" + funcHor.calcGratificacao() + "]");
-        System.out.println("Salário Líquido: [" + funcHor.calcSalLiquido() + "]");
+        //INSTACIA E PASSA VALORES A CLASSE FuncionarioComissionado
+        FuncionarioComissionado funcCom = new FuncionarioComissionado(3030, "Jorge", "08/12/2007", 10);
+        funcCom.setSalBase(1200);
+        funcCom.setCargo("Vendedor");
+        funcCom.addVendas(12056.56);
         
+        //IMPRIME OS VALORES DA CLASSE FuncionarioHorista
+        System.out.println("HORISTA");
+        System.out.println("Cargo: [" + funcHor.getCargo() + "]");
+        System.out.println("Salario Bruto: [" + df.format(funcHor.calcSalBruto()) + "]");
+        System.out.println("Desconto: [" + df.format(funcHor.calcDesconto()) + "]");
+        System.out.println("Gratificação: [" + df.format(funcHor.calcGratificacao()) + "]");
+        System.out.println("Salário Líquido: [" + df.format(funcHor.calcSalLiquido()) + "]");
+        
+        //IMPRIME OS VALORES DA CLASSE FuncionarioMensalista
         System.out.println("\nMENSALISTA");
         System.out.println("Cargo: [" + funcMen.getCargo() + "]");
-        System.out.println("Salario Bruto: [" + funcMen.calcSalBruto() + "]");
-        System.out.println("Desconto: [" + funcMen.calcDesconto() + "]");
-        System.out.println("Salário Líquido: [" + funcMen.calcSalLiquido() + "]");
+        System.out.println("Salario Bruto: [" + df.format(funcMen.calcSalBruto()) + "]");
+        System.out.println("Desconto: [" + df.format(funcMen.calcDesconto()) + "]");
+        System.out.println("Salário Líquido: [" + df.format(funcMen.calcSalLiquido()) + "]");
+        
+        //IMPRIME OS VALORES DA CLASSE FuncionarioComissionado
+        System.out.println("\nCOMISSIONADO");
+        System.out.println("Cargo: [" + funcCom.getCargo() + "]");
+        System.out.println("Salario Bruto: [" + df.format(funcCom.calcSalBruto()) + "]");
+        System.out.println("Desconto: [" + df.format(funcCom.calcDesconto()) + "]");
+        System.out.println("Gratificação: [" + df.format(funcCom.calcGratificacao()) + "]");
+        System.out.println("Taxa Comissão: [" + df.format(funcCom.getTaxaComissao()) + "]");
+        System.out.println("Total Vendas: [" + df.format(funcCom.getTotalVendas()) + "]");
+        System.out.println("Salário Líquido: [" + df.format(funcCom.calcSalLiquido()) + "]");
     }
     
 }
